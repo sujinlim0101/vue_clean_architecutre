6db8880118bafd9e3a1686343a4a375a1cc10bb7
Sujin Lim <sjlim@quriously.com>
2021-06-20 00:51:56 +0900

    Add weather component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <Weather />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Weather from './components/Weather.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    Weather
   }
 });
 </script>
---
+++ src/components/Weather.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>Today's weather</div>
+  <div>🇰🇷 Seoul: {{ seoulCurrentWeather }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import axios from 'axios';
+
+export default defineComponent({
+  data() {
+    return {
+      seoulCurrentWeather: Object
+    }
+  },
+  mounted() {
+    axios.get('http://api.openweathermap.org/data/2.5/weather?lat=37.56826&lon=126.977829&APPID=' + process.env.VUE_APP_API_KEY)
+      .then(response => {
+        console.log('response', response);
+        this.seoulCurrentWeather = response.data.main;
+      })
+  },
+})
+
+</script>
+
+<style>
+
+</style>
