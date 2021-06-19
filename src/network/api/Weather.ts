cabbfb3d16f172e60734e9ca0ecd9ed942f49a33
Sujin Lim <sjlim@quriously.com>
2021-06-20 00:51:03 +0900

    Add api & network

---
+++ src/network/api/Weather.ts
@@ -0,0 +1,7 @@
+import { AxiosResponse } from 'axios'
+import { APIRequest } from '@/network/APIRequest';
+import { HTTPMethod } from '@/network/APIClient';
+
+export namespace WeatherAPI {
+  //export class GetTodayWeather implements APIRequest<>
+}
---
+++ src/network/apiClient.ts
@@ -0,0 +1,64 @@
+import axios from 'axios';
+import { APIResponse } from './APIResponse';
+import { APIRequest } from './APIRequest';
+import { APIError } from './APIError';
+import { NetworkClient } from './NetworkClient';
+
+export enum HTTPMethod {
+  GET = 'GET',
+  POST = 'POST',
+  DELETE = 'DELETE',
+  PUT = 'PUT'
+}
+
+export class APIClient implements NetworkClient{
+  static shared = new APIClient();
+
+  baseURL: string = 'http://api.openweathermap.org/data/2.5';
+  timeout: number = 15 * 1000;
+
+  request<U extends APIResponse>(request: APIRequest<U>): Promise<U> {
+    const isRead = request.method === HTTPMethod.GET;
+
+    return new Promise<U>((resolve, reject) => {
+      axios
+        .request({
+          url: request.path,
+          method: request.method,
+          params: isRead && request.params,
+          data: !isRead && request.params,
+          withCredentials: true,
+          timeout: this.timeout,
+          baseURL: request.baseURL || this.baseURL,
+          headers: this.createHeaders()
+        })
+        .then(data => {
+          const response = request.parse
+            ? request.parse(data)
+            : this.parse<U>(data);
+          resolve(response);
+          console.log('response', response);
+        })
+        .catch(err => {
+          const apiError = this.normalizeError(err);
+          reject(apiError);
+        });
+    });
+  }
+
+  private parse<U extends APIResponse>(data: any): U {
+    return data;
+  }
+
+  private normalizeError(error: any): APIError {
+    return {
+      status: error.response && error.response.status,
+      message: error.message,
+      raw: error
+    };
+  }
+
+  private createHeaders(): any {
+    return {};
+  }
+}
