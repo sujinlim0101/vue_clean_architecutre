12c850befaf801a78aa188ec07814875b0e9f0ae
Sujin Lim <sjlim@quriously.com>
2021-06-20 13:25:04 +0900

    Add repositories for use apiClient

--- src/network/api/Weather.ts
+++ src/network/api/Weather.ts
@@ -1,7 +1,16 @@
 import { AxiosResponse } from 'axios'
 import { APIRequest } from '@/network/APIRequest';
-import { HTTPMethod } from '@/network/APIClient';
-
+import { HTTPMethod } from '@/network/apiClient';
+import { IWeather } from '@/entities/Weather';
 export namespace WeatherAPI {
-  //export class GetTodayWeather implements APIRequest<>
+  export class GetWeather implements APIRequest<IWeather> {
+    response!: IWeather;
+    path = '/weather';
+    method = HTTPMethod.GET;
+    parse = (data: AxiosResponse) => data.data
+    // TODO: move city to
+    constructor(city: string) {
+      this.path = `/weather?q=${city}&appid=${process.env.VUE_APP_API_KEY}`;
+    }
+  }
 }
