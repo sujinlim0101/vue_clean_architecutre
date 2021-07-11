0ac1687d630c049e2421dec1476f9189f49dc0d0
Sujin Lim <a01041401160@gmail.com>
2021-07-11 22:16:42 +0900

    Add bootstrap and change css

--- src/container/weather/index.vue
+++ src/container/weather/index.vue
@@ -1,12 +1,17 @@
 <template>
-  <div>
-    <h2>Enter city for searching current weather !</h2>
+  <div class="container">
+    <h2>Enter city for searching weather !</h2>
     <form @submit.prevent="sendForm">
-      <input v-model='city'>
-      <button type='submit'>조회하기</button>
+      <label for="city" class="visually-hidden">City</label>
+      <div class="input__wrapper">
+        <input id="city" class="form-control" type="text" placeholder="Enter City" aria-label="input for city" v-model='city'>
+      </div>
+      <div>
+        <button type="submit" class="btn btn-primary mb-3">조회하기</button>
+      </div>
     </form>
-    <div class="weather__container">
-      <h4 class="result" v-if="searchedCity">{{ searchedCity }}의 날씨</h4>
+    <div class="weather__container" v-if="weather">
+      <h4 class="result__title" v-if="searchedCity">👒 {{ searchedCity }}'s Today Weather</h4>
       <table>
         <tr>
           <th v-for="(value, name) in weather" v-bind:key="value">{{ titleToKor[name] }}</th>
@@ -16,9 +21,9 @@
         </tr>
       </table>
     </div>
-    <div>
-      <h4 class="result" v-if="forecast.length > 0">{{ searchedCity }}의 예보</h4>
-      <table v-if="forecast.length > 0">
+    <div class="weather__container" v-if="forecast.length > 0">
+      <h4 class="result__title">🧢 {{ searchedCity }}'s Forecast</h4>
+      <table>
         <tr>
           <th v-for="(value, name) in forecast[0]" v-bind:key="value">{{ titleToKor[name] }}</th>
         </tr>
@@ -55,9 +60,6 @@ export default defineComponent({
       }
     }
   },
-  mounted () {
-    this.weather = {}
-  },
   methods: {
     async getWeather() {
       this.weather = await new SearchWaetherUseCase(this.city).execute();
@@ -75,18 +77,34 @@ export default defineComponent({
 
 </script>
 
-<style>
-.result {
-  margin-top: 2rem;
+<style lang="scss">
+.container {
+  padding: 1rem;
+}
+.result__title {
+  margin: 1.2rem;
+  font-size: 2.2rem;
+  font-weight: 600;
 }
 table {
   margin: 0 auto;
   border-collapse: collapse;
 }
+form {
+  display: flex;
+  justify-content: space-between;
+  max-width: 700px;
+  margin: 0 auto;
+}
+.input__wrapper {
+  flex-grow: 1;
+  padding-right: 1rem;
+}
 td, th {
-  padding: 16px;
+  padding: 1rem;
   text-align: left;
-  border: 1px solid gray;
+  border: 1px solid #326a64 !important;
+  border-width: 1px !important;
 }
 .weather__container {
   max-width: 700px;
