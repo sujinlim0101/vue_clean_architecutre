2f7ada17fcc1f1c74312cfba8c5a3adc408dca99
Sujin Lim <a01041401160@gmail.com>
2022-03-01 11:19:44 +0900

    Fix http -> https

--- src/network/apiClient.ts
+++ src/network/apiClient.ts
@@ -14,7 +14,7 @@ export enum HTTPMethod {
 export class APIClient implements NetworkClient{
   static shared: NetworkClient = new APIClient();
 
-  baseURL: string = 'http://api.openweathermap.org/data/2.5';
+  baseURL: string = 'https://api.openweathermap.org/data/2.5';
   timeout: number = 15 * 1000;
 
   request<U extends APIResponse>(request: APIRequest<U>): Promise<U> {
